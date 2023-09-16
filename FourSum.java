ef99de29bf5d9763cbbbf0ae1fc0527a284f4ddc
ozzy ozbourne <khanosaid726@gmail.com>
2023-09-16 23:22:02 +0530

    updated soln

--- FourSum.java
+++ FourSum.java
@@ -4,23 +4,22 @@ import java.util.List;
 
 public final class FourSum {
 
-    private final List<List<Integer>> res = new ArrayList<>();
-    private final List<Integer> quad = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println(new FourSum().fourSum(new int[]{1,0,-1,0,-2,2}, 0));
+    }
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
         if (nums.length < 4) return new ArrayList<>();
         Arrays.sort(nums);
-        kSum(nums, 4, 0, target);
-        return res;
+        return ksum(nums, 4, 0, target, new ArrayList<>(), new int[2]);
     }
 
-    private void kSum(int[] nums, int k, int start, long target){
+    private List<List<Integer>> ksum(int[] nums, int k, int start, long target, List<List<Integer>> res, int[] tuple )  {
         if (k > 2){
             for(int i = start; i < nums.length-k+1; i++){
                 if (i > start && nums[i] == nums[i-1])continue;
-                quad.add(nums[i]);
-                kSum(nums, k-1, i+1, target-nums[i]);
-                quad.remove(quad.size()-1);
+                tuple[4-k] = nums[i];
+                res = ksum(nums, k-1, i+1, target-nums[i], res, tuple);
             }
         }else {
             int left = start, right = nums.length-1;
@@ -29,12 +28,11 @@ public final class FourSum {
                 if (sum < target)left+=1;
                 else if (sum > target)right-=1;
                 else {
-                    res.add(List.of(quad.get(0), quad.get(1), nums[left], nums[right]));
+                    res.add(List.of(tuple[0], tuple[1], nums[left], nums[right]));
                     left+=1;
                     while (nums[left] == nums[left -1] && left < right)left+=1;
-
                 }
             }
-        }
+        }return res;
     }
 }
