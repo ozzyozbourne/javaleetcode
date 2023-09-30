2f55921bef138753679176ef52434faa310acc90
ozzy ozbourne <khanosaid726@gmail.com>
2023-09-30 05:46:39 +0530

    just one fix

--- Subsequences.java
+++ Subsequences.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public final class Subsequences {
@@ -6,20 +7,23 @@ public final class Subsequences {
     private Subsequences(){}
 
     public static void main(String[] args) {
-        var d1 = new ArrayList<Integer>();
-        d1.add(3);
-        d1.add(2);
-        d1.add(1);
-        for(var a: subsequence(d1)) System.out.printf("%s\n", a);
+        var d1 = List.of(1,2,3);
+        System.out.println(subsequence(new ArrayList<>(), d1, 0));
     }
 
-    private static List<List<Integer>> subsequence(List<Integer> d1){
-        if(d1.isEmpty()) {
-            List<List<Integer>> d2 = new ArrayList<>();
+    private static List<List<Integer>> subsequence(List<List<Integer>>d2, List<Integer> d1, int i){
+        if(i == d1.size()) {
             d2.add(new ArrayList<>());
             return d2;
         }
-        return  subsequence(d1.subList(1, d1.size()));
+        var noHead = subsequence(d2, d1, i+1);
+        var head = new ArrayList<List<Integer>>();
+        noHead.forEach(s -> {
+            var tobeAdded = new ArrayList<>(s);
+            tobeAdded.add(d1.get(i));
+            head.add(tobeAdded);});
+        noHead.addAll(head);
+        return noHead;
     }
     
 }
